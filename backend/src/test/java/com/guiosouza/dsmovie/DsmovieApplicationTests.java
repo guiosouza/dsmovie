41ab862c81416d3fdbfabf30037f18815d585268
unknown <guilhermedeoliveiradesouza@gmail.com>
2023-07-18 16:51:40 -0300

    Project created

---
+++ backend/src/test/java/com/guiosouza/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiosouza.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
