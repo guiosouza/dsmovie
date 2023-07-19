a9863779fe6dd2eda0ca3ad47e1a131c36de2ae1
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2023-07-19 16:05:47 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div><h1>Página de formulário</h1></div>
+    )
+}
+
+export default Form
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Listing() {
+    return (
+        <div><h1>Página de listagem</h1></div>
+    )
+}
+
+export default Listing;
